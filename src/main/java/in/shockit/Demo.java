07f2a2f1a3df9caeec7493c0f7192c5c838d4076
Pavan <pavanwadbudhe1998@gmail.com>
2024-03-21 15:04:53 +0530

    privous BUG-1345 which are stashed are completed

--- src/main/java/in/shockit/Demo.java
+++ src/main/java/in/shockit/Demo.java
@@ -5,6 +5,12 @@ public class Demo {
 	public void m2() {
 		//BUG-9989 fixed 
                 //logic completed 
+        }
+
+	public void m1() {
+		//ligic for BUG-1345
+               // Now completed
+
 	}
 
 }
